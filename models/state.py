892950c783d9f6b7be965c3d87a6b1899149a454
Tayebwa-ian <tayebwaian0@gmail.com>
2024-03-25 03:48:53 +0300

    fix errors

--- models/state.py
+++ models/state.py
@@ -17,7 +17,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
         cities = relationship("City", backref="state",
-                            cascade="all, delete-orphan")
+                              cascade="all, delete-orphan")
 
     def __init__(self, *args, **kwargs):
         """initializes state"""
